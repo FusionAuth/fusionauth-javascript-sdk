994444b840667b1b6121231e6e0d8c24155c42de
david-chalk <dprae@chalk.inc>
2024-03-11 11:00:57 -0600

    Setup new React SDK package using Vite

---
+++ packages/sdk-react-v2/src/components/withFusionAuth.tsx
@@ -0,0 +1,31 @@
+import type { ComponentType } from 'react';
+import {
+    FusionAuthContext,
+    IFusionAuthContext,
+} from '../providers/FusionAuthProvider';
+
+export interface WithFusionAuthProps {
+    fusionAuth: IFusionAuthContext;
+}
+
+export const withFusionAuth = <
+    Props extends WithFusionAuthProps = WithFusionAuthProps,
+>(
+    Component: ComponentType<Props>,
+) => {
+    const displayName = Component.displayName;
+
+    const FusionAuthComponent = (
+        props: Omit<Props, keyof WithFusionAuthProps>,
+    ) => (
+        <FusionAuthContext.Consumer>
+            {(fusionAuth: IFusionAuthContext) => (
+                <Component {...(props as Props)} fusionAuth={fusionAuth} />
+            )}
+        </FusionAuthContext.Consumer>
+    );
+
+    FusionAuthComponent.displayName = displayName;
+
+    return FusionAuthComponent;
+};
---
+++ packages/sdk-react-v2/vite.config.ts
@@ -0,0 +1,32 @@
+
+// <reference types="vitest" />
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+// import dts from 'vite-plugin-dts'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime']
+    }
+  },
+  plugins: [
+    react()
+    // dts({ rollupTypes: true }),
+  ],
+  // test: {},
+})
