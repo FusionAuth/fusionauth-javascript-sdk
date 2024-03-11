730811e584d007c048125f5b4c51ecd703690e60
david-chalk <dprae@chalk.inc>
2024-03-11 16:34:38 -0600

    Migrate FusionAuthLogoutButton test to new React SDK

---
+++ packages/sdk-react/src/components/ui/FusionAuthLogoutButton/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { useFusionAuth } from '../providers/FusionAuthProvider';
+import styles from '../../styles/button.module.scss';
+import classNames from 'classnames';
+
+interface Props {
+    text?: string;
+    className?: string;
+}
+
+export const FusionAuthLogoutButton: FC<Props> = ({ text, className }) => {
+    const { logout } = useFusionAuth();
+
+    return (
+        <button
+            className={classNames(styles.fusionAuthButton, className)}
+            type="button"
+            onClick={() => logout()}
+        >
+            {text ?? 'Logout'}
+        </button>
+    );
+};
