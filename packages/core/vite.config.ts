b409b97db0865a8e4e397f325d7b38e5bc0793db
Jake Loew <jake@madebychalk.com>
2024-03-27 16:11:15 -0600

    Set up vitest in core package

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,8 +1,12 @@
-import { resolve } from 'path';
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 export default defineConfig({
+  test: {
+    environment: 'jsdom',
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
