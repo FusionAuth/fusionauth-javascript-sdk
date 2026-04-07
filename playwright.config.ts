ad34fab856d42c69216875051fcaf33cfaa20247
Mike Rudat <mike.rudat@fusionauth.io>
2026-04-07 14:26:09 -0600

    Opt out of parallel tests as the same FusionAuth user is being used in every test.

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 9,
+  workers: 1,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
